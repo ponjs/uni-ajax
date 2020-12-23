15fccf8d1f5ddf5691ba93f4dacfb59dd29efe2c
ponjs <2552658135@qq.com>
2020-12-23 11:32:06 +0800

    chore(webpack): webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'ajax',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({ patterns: [path.resolve(__dirname, 'src', 'index.d.ts')] })
+  ]
+}
