ab13db4f6b8a34a4e95dbe51e3014b31dfe7070b
ponjs <2552658135@qq.com>
2021-02-24 11:28:19 +0800

    chore(package): 编译自动修改uni_modules版本号

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import fs from 'fs'
 import babel from '@rollup/plugin-babel'
 import copy from 'rollup-plugin-copy'
 import { version } from './package.json'
@@ -15,6 +16,13 @@ const target = process.env.TARGET === 'uni' ? 'example/uni_modules/u-ajax/js_sdk
 const copyTargets = [{ src: 'src/index.d.ts', dest: path.resolve(target) }]
 if (process.env.TARGET === 'uni' && process.env.BUILD === 'release') {
   copyTargets.push({ src: 'README.md', dest: path.resolve(target, '..') })
+
+  const filePath = path.resolve(target, '../package.json')
+  fs.readFile(filePath, (err, data) => {
+    const json = JSON.parse(data.toString())
+    json.version = version
+    fs.writeFileSync(filePath, JSON.stringify(json, null, '\t'))
+  })
 }
 
 export default {
